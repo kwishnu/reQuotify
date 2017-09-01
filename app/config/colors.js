c3ee0ebb04b436242dff425e4136218d06c40c3c
kwishnu <kwish1777@gmail.com>
2017-09-01 12:28:51 -0700

    Added color

--- app/config/colors.js
+++ app/config/colors.js
@@ -3,7 +3,8 @@ export const colors = {
     light_blue: '#33b5e5',
     blue_bg: '#060221',//'#070f4e',//'#0f0747',
     pale_bg: '#c9c9a7',
-    home_border: '#070f4e'
+    home_border: '#070f4e',
+    medium_gray: '#484848'
 
 };
 
